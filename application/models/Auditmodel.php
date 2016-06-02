65f385698cbf915c537be941cee026fc676159aa
minchit <minchitstar@gmail.com>
2016-06-02 15:10:53 +0630

    Clean up Audit Model

--- application/models/Auditmodel.php
+++ application/models/Auditmodel.php
@@ -11,21 +11,7 @@ class Auditmodel extends CI_Model
 		print_r($data);
 		echo "</pre>";
 	}
-	
-	
-	
-	function test_log()
-	{
-		$this->db->select('cr.*');
-		$this->db->from('ox_cr_list as cr');
-		$this->db->order_by('cr_id','desc');
-		$this->db->limit(1);
-		//$this->db->where('u.user_id',$u);
-		//$this->db->where('u.password',$p);
-		$query = $this->db->get();
-		return $query->result();
-	}
-	
+
 	function insert_audit_trn($data)
 	{
 		$this->db->insert('audit_trn', $data);
@@ -48,16 +34,7 @@ class Auditmodel extends CI_Model
 		$this->db->insert('audit_trn',$data);
 	}
 	
-	function audit_tbl_insert($data)
-	{
-		$this->db->insert('audit_tbl',$data);
-		
-	}
 	
-	function audit_fld_insert($data)
-	{
-		$this->db->insert('audit_fld',$data);
-	}
 	
 	function last_tran_seq_no()
 	{
@@ -80,20 +57,7 @@ class Auditmodel extends CI_Model
 		}
 				
 	}
-	
-	function last_table_seq_no()
-	{
-	
-		$this->db->select('tab.*');
-		$this->db->from('audit_tbl as tab');
-		$this->db->order_by('table_seq_no','desc');
-		$this->db->limit(1);
-		//$this->db->where('u.user_id',$u);
-		//$this->db->where('u.password',$p);
-		$query = $this->db->get();
-		return $query->result();
-	}
-	
+
 	function get_p_key($table)
 	{
 		$fields = $this->db->field_data($table);
@@ -128,164 +92,5 @@ class Auditmodel extends CI_Model
 		}else{
 			return 0;
 		}
-	}
-	
-	function update_cr($table,$con,$data){
-		$this->db->where($con);
-		if($this->db->update($table,$data)){
-			return 1;
-		}else{
-			return 0;
-		}
-	}
-	
-	function selectcr($cr_id)
-	{
-		if(is_array($cr_id) && $_SERVER['REQUEST_METHOD'] == 'POST')
-		{
-			
-			
-			//$this->pr($_SERVER['REQUEST_METHOD']);
-			//$this->pr($_SESSION['radio']);
-			
-			if($_SESSION['radio']=='CR_ID')
-			{
-				
-				if(!empty($cr_id['cr_id_search']))
-				{
-					$this->db->select('cr.*');
-					$this->db->from('ox_cr_list as cr');
-					$this->db->like('cr.cr_id', $cr_id['cr_id_search']);
-					$query=$this->db->get();
-					return $query->result();
-				}
-			}
-			else if($cr_id['radiovalue']=='CR_ID_Range')
-			{
-				
-				if(empty($cr_id['cr_id_from']) || empty($cr_id['cr_id_to']))
-				{
-					
-					$this->db->select('cr.*');
-					$this->db->from('ox_cr_list as cr');
-					if(!empty($cr_id['cr_id_from']))
-					{
-						$this->db->where('cr.cr_id >',$cr_id['cr_id_from']);
-							
-					}
-					else if(!empty($cr_id['cr_id_to']))
-					{
-							
-						$cr_id_to=implode('', $cr_id);
-						//$this->pr($cr_id_to);
-						$this->db->where('cr.cr_id >',$cr_id_to);
-							
-					}
-					$query=$this->db->get();
-					return $query->result();
-				}
-				else
-				{
-					
-					$this->db->select('cr.*');
-					$this->db->from('ox_cr_list as cr');
-					$this->db->where('cr.cr_id >=',$cr_id['cr_id_from']);
-					$this->db->where('cr.cr_id <=',$cr_id['cr_id_to']);
-					$query=$this->db->get();
-					return $query->result();
-				
-				}
-			}
-			else if($_SESSION['radio']=='Submitted_Date')
-			{
-				//$this->pr($_SESSION['radio']);
-				//exit();
-				if(!empty($cr_id['submit_date']))
-				{
-					//$this->pr($_SESSION['radio']);
-					
-					$this->db->select('cr.*');
-					$this->db->from('ox_cr_list as cr');
-					$this->db->where('cr.cr_submitted',$cr_id['submit_date']);
-					$query=$this->db->get();
-					return $query->result();
-				}
-			}
-			else if($_SESSION['radio']=='Requestor')
-			{
-				//$this->pr($_SESSION['radio']);
-				//exit();
-				if(!empty($cr_id['request']))
-				{
-					$this->db->select('cr.*');
-					$this->db->from('ox_cr_list as cr');
-					$this->db->like('cr.cr_requestor', $cr_id['request']);
-					$query=$this->db->get();
-					return $query->result();
-				}
-			}
-			else if($_SESSION['radio']=='Processed_Date')
-			{
-				//$this->pr($_SESSION['radio']);
-				//exit();
-				if(!empty($cr_id['process_date']))
-				{
-					$this->db->select('cr.*');
-					$this->db->from('ox_cr_list as cr');
-					$this->db->where('cr.cr_status_processed',$cr_id['process_date']);
-					$query=$this->db->get();
-					return $query->result();
-				}
-			}
-			else if($_SESSION['radio']=='Processed_By')
-			{
-				//$this->pr($_SESSION['radio']);
-				//exit();
-				if(!empty($cr_id['process_by']))
-				{
-					$this->db->select('cr.*');
-					$this->db->from('ox_cr_list as cr');
-					$this->db->where('cr.cr_processed_by',$cr_id['process_by']);
-					$query=$this->db->get();
-					return $query->result();
-				}
-			}
-					
-		}
-		else
-		{
-			$this->db->select('cr.*');
-			$this->db->from('ox_cr_list as cr');
-			$this->db->where('cr.cr_id',$cr_id);
-			
-			$query=$this->db->get();
-			return $query->result();
-		}
-	}
-	
-	function getcr()
-	{
-	
-		$this->db->select('cr.*');
-		$this->db->from('ox_cr_list as cr');
-		$this->db->order_by('cr_id','desc');
-		$this->db->limit(20);
-		//$this->db->where('u.user_id',$u);
-		//$this->db->where('u.password',$p);
-		$query = $this->db->get();
-		return $query->result();
-	}
-	
-	function lastcr()
-	{
-	
-		$this->db->select('cr.*');
-		$this->db->from('ox_cr_list as cr');
-		$this->db->order_by('cr_id','desc');
-		$this->db->limit(1);
-		//$this->db->where('u.user_id',$u);
-		//$this->db->where('u.password',$p);
-		$query = $this->db->get();
-		return $query->result();
 	}
 }
